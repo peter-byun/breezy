55540330f2aebc72da465ad71fc39ba6c483b171
peter-byun <peter1035k@gmail.com>
2025-03-25 22:55:50 +0900

    fix(dnd): delete dead code

--- apps/web/src/features/dnd/utilities/createRange.ts
+++
@@ -1,8 +0,0 @@
-const defaultInitializer = (index: number): number => index;
-
-export function createRange<T = number>(
-  length: number,
-  initializer: (index: number) => T = defaultInitializer as (index: number) => T
-): T[] {
-  return Array.from({ length }, (_, index) => initializer(index));
-}
