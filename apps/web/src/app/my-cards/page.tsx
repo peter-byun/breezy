2fb5be6eacab4e27d3ca97d192e918c9f2f1efc0
peter-byun <peter1035k@gmail.com>
2025-04-03 21:00:03 +0900

    refactor(my-card): remove unnecessary abstraction

--- apps/web/src/app/my-cards/page.tsx
+++ apps/web/src/app/my-cards/page.tsx
@@ -4,13 +4,16 @@ import { PageLayout } from "@/layouts/page-layout/PageLayout";
 import { TopNavBar } from "@/layouts/nav-bar/TopNavBar";
 import { ManageableCardList } from "./components/ManageableCardList/ManageableCardList";
 import { AddCardRoot } from "./components/AddCard/AddCardRoot";
+import { useCard } from "@/features/card/useCard";
 
 export default function MyCards() {
+  const { deleteCard } = useCard();
+
   return (
     <PageLayout>
       <TopNavBar />
       <AddCardRoot />
-      <ManageableCardList />
+      <ManageableCardList onDeleteCardClick={deleteCard} />
     </PageLayout>
   );
 }
