437decad1562099053969e977d6e615cafa463c7
peter-byun <peter1035k@gmail.com>
2025-04-15 22:52:52 +0900

    test(web): add a test provider for libraries and suspense

---
+++ apps/web/src/__tests__/components/TestPageLoader.tsx
@@ -0,0 +1,17 @@
+import ReactQueryProvider from "@/layouts/library-provider/ReactQueryProvider";
+import { Theme } from "@radix-ui/themes";
+import { Suspense } from "@suspensive/react";
+import { OverlayProvider } from "@toss/use-overlay";
+import { PropsWithChildren } from "react";
+
+export const TestPageLoader = ({ children }: PropsWithChildren) => {
+  return (
+    <Theme>
+      <OverlayProvider>
+        <ReactQueryProvider>
+          <Suspense fallback={<p>loading</p>}>{children}</Suspense>
+        </ReactQueryProvider>
+      </OverlayProvider>
+    </Theme>
+  );
+};
