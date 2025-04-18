24b0133d91923e160e24728176046391038f49e5
peter-byun <peter1035k@gmail.com>
2025-04-18 21:54:29 +0900

    refactor(user): make the page error fallback component extendable

--- apps/web/src/ui-components/error/PageErrorFallback.tsx
+++ apps/web/src/ui-components/error/PageErrorFallback.tsx
@@ -1,39 +1,44 @@
-import { TopNavBar } from "@/layouts/nav-bar/TopNavBar";
+import { TopNavBar } from "@/features-shared/top-nav-bar/TopNavBar";
 import { PageLayout } from "@/layouts/page-layout/PageLayout";
 import { css } from "@emotion/react";
 import { Flex } from "@radix-ui/themes";
 import { Text } from "@radix-ui/themes/components/callout";
 import Link from "next/link";
+import { PropsWithChildren } from "react";
 
-export const PageErrorFallback = () => {
+export const PageErrorFallback = ({ children }: PropsWithChildren) => {
   return (
     <PageLayout>
       <TopNavBar />
-      <Flex
-        width={"100%"}
-        height={"100%"}
-        direction={"column"}
-        align={"center"}
-        justify={"center"}
-        gap={"4"}
-      >
-        <h2>Something went wrong</h2>
-        <Text align={"center"}>
-          If the error persists even after refreshing the website, <br />
-          please contact us via{" "}
-          <Link
-            href="mailto:pixelperfectprogramming@gmail.com"
-            css={css`
-              color: #2471ff;
-            `}
-          >
-            pixelperfectprogramming@gmail.com
-          </Link>
-          .
-          <br />
-          We will fix it as soon as possible.
-        </Text>
-      </Flex>
+      {children ? (
+        children
+      ) : (
+        <Flex
+          width={"100%"}
+          height={"100%"}
+          direction={"column"}
+          align={"center"}
+          justify={"center"}
+          gap={"4"}
+        >
+          <h2>Something went wrong</h2>
+          <Text align={"center"}>
+            If the error persists even after refreshing the website, <br />
+            please contact us via{" "}
+            <Link
+              href="mailto:pixelperfectprogramming@gmail.com"
+              css={css`
+                color: #2471ff;
+              `}
+            >
+              pixelperfectprogramming@gmail.com
+            </Link>
+            .
+            <br />
+            We will fix it as soon as possible.
+          </Text>
+        </Flex>
+      )}
     </PageLayout>
   );
 };
